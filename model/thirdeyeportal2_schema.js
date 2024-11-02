205081eeef7eb4efebe109b5d0f624142b3bb084
database-onyx <141583093+database-onyx@users.noreply.github.com>
2024-11-02 13:06:29 +0530

    initial commit

---
+++ model/thirdeyeportal2_schema.js
@@ -0,0 +1,22 @@
+const { text } = require("body-parser");
+const mongoose = require("mongoose");
+
+const imageSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    number: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    image: {
+        data: Buffer,
+        contentType: String
+            // required: true
+    }
+});
+
+
+const ImageModel1 = mongoose.model("portal10", imageSchema,"portal10");
+module.exports = ImageModel1;
